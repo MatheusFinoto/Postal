69ac35cd42ea31f8aa0733dbbfbf46faa558f1c2
MatheusFinoto <matheus.finoto@hotmail.com>
2018-09-20 14:06:39 -0300

    Initial commit

---
+++ app/src/main/java/postal/fisan/com/postal/LoginActivity.java
@@ -0,0 +1,38 @@
+package postal.fisan.com.postal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText txtEmail, txtSenha;
+    private Button btnLogin;
+    private CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+        checkBox = findViewById(R.id.checkbox);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, IndexActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
